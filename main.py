d7d04884c603489caa97c0bf772e96d54255ea1a
t-gappy <t-gappy@fuji.waseda.jp>
2021-10-30 01:21:34 +0900

    fix CORS error from front

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from src.dialogue_model import make_reply
-
+from starlette.middleware.cors import CORSMiddleware
 
 class Memo(BaseModel):
     input_text: str
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.post("/")
 def reply_to_memo(memo: Memo):
